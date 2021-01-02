8d354bc790fc4725b38a4ebd2889e11b71435429
Umesh Shukla <umesh.shukla@gaana.com>
2021-01-03 01:15:24 +0530

    allure environment setup added

---
+++ src/main/java/utils/AllureListener.java
@@ -0,0 +1,29 @@
+package utils;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import common.GlobalConfigHandler;
+import java.util.Properties;
+import org.testng.IExecutionListener;
+
+public class AllureListener implements IExecutionListener {
+    
+    public void onExecutionFinish() {
+        Properties prop = new Properties();
+        String FILE_PATH = System.getProperty("user.dir") + "/allure-results/environment.properties";
+        try{
+            OutputStream outputStream = new FileOutputStream(FILE_PATH);
+            prop.setProperty("Organization Name", "GGL");
+            prop.setProperty("QA Name", "Umesh Shukla");
+            prop.setProperty("Os Name", System.getProperty("os.name"));
+            prop.setProperty("User Name", System.getProperty("user.name"));
+            prop.setProperty("Host Name", InetAddress.getLocalHost().getHostName());
+            prop.setProperty("Java Version", System.getProperty("java.version"));
+            prop.setProperty("Execution Context", GlobalConfigHandler.getEnv());
+            prop.store(outputStream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
