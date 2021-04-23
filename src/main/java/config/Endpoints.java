2c3ad0065c19414da8b32c7c03661c6281b8c0d9
Umesh Shukla <umesh.shukla@gaana.com>
2021-04-23 14:49:44 +0530

    changes for api endpoint

--- src/main/java/config/Endpoints.java
+++ src/main/java/config/Endpoints.java
@@ -17,7 +17,7 @@ public class Endpoints {
     public static String searchFeed = "/searchfeed/fetch";
     public static String autoSuggestStageEndpoint(String query_params) {
         StringBuilder endpoint = new StringBuilder();
-        endpoint.append("/gaanasearch-api/mobilesuggest/autosuggest-lite-vltr-ro?query="+query_params);
+        endpoint.append("/gaanasearch-api-vi1/mobilesuggest/autosuggest-lite-vi1?query="+query_params);
         endpoint.append("&UserType=0&geoLocation=IN&content_filter=2&include=allItems&isRegSrch=0&usrLang=Hindi,English,Punjabi&testing=1");
         return endpoint.toString();
     }
@@ -31,7 +31,7 @@ public class Endpoints {
 
     public static String autoSuggestSolrEndpoint(String query_params) {
         StringBuilder endpoint = new StringBuilder();
-        endpoint.append("/gaanasearch-api/mobilesuggest/getErSolr?query=" + query_params);
+        endpoint.append("/gaanasearch-api-vi1/mobilesuggest/getErSolr?query=" + query_params);
         endpoint.append("&UserType=0&geoLocation=IN&content_filter=2&include=allItems&isRegSrch=0&usrLang=Hindi,English,Punjabi&testing=1");
         return endpoint.toString();
     }
