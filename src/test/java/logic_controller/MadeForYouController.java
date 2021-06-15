d24da34a9ef1f4e22578bb7648c49d08e3b8201b
Umesh Shukla <umesh.shukla@gaana.com>
2021-06-15 07:49:27 +0530

    made for you code updated

---
+++ src/test/java/logic_controller/MadeForYouController.java
@@ -0,0 +1,9 @@
+package logic_controller;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
+public class MadeForYouController {
+    
+    // private static Logger log = LoggerFactory.getLogger(MadeForYouController.class);
+
+}
